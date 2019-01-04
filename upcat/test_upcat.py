45fd558cbd5f2495b757524df284abfa7faebe9f
Anthony Chao <anthony.chao@blackrock.com>
2019-01-03 17:08:07 -0800

    Added add and delete methods to upcat.py

---
+++ upcat/test_upcat.py
@@ -0,0 +1,19 @@
+"""
+Testing Suite for upcat.py
+"""
+
+
+import unittest
+
+
+class TestMethods(unittest.TestCase):
+    """Collection of tests for upcat.py"""
+
+    # assertEqual()
+    # assertTrue()
+    # assertFalse()
+    # assertRaises()
+
+
+if __name__ == "__main__":
+    unittest.main()
--- upcat/upcat.py
+++ upcat/upcat.py
@@ -18,6 +18,101 @@ PRIMARY_KEY_FILES = ["Centers.catalog.test",
 COMPOSITE_KEY_FILES = ["Services.catalog.test"]
 
 
+def add(args):
+    """Adds new key row to file."""
+
+    if args.file in PRIMARY_KEY_FILES:
+        with open(args.file, 'r') as infile:
+            keys = get_keys(args.file)
+
+            if args.key in keys:
+                print("Provided key: '{}' already exists in {}, EXITING.".
+                      format(args.key, args.file))
+                exit(1)
+            infile.seek(0)
+
+            file_header_length = len(infile.readline().split('|'))
+            data = [''] * file_header_length
+            data[0] = args.key
+            data = '|'.join(data) + '\n'
+        with open(args.file, 'a') as infile:
+            infile.write(data)
+        print("Added '{}' to {}.".
+              format(args.key, args.file))
+
+    elif args.file in COMPOSITE_KEY_FILES:
+        # TODO (achao): Handle composite key for Services.catalog
+        pass
+
+    else:
+        print("Please provide a valid catalog filename.")
+        print("Valid files: {}".
+              format(PRIMARY_KEY_FILES + COMPOSITE_KEY_FILES))
+        exit(1)
+
+
+def read(args):
+    """Prints header and values for given key."""
+
+    if args.file in PRIMARY_KEY_FILES:
+        infile = open(args.file, 'r')
+
+        # Get list of keys from file and return cursor to start of file
+        keys = get_keys(args.file)
+
+    # TODO (achao): Figure out what to do with duplicates
+
+    #    from collections import Counter
+    #    print(Counter(keys))
+    #    for key in list(Counter(keys)):
+    #        print(key, end="")
+
+        infile.seek(0)
+
+        # If optional key argument is not provided, print all lines of file
+        line = infile.readline()
+        if args.key is None:
+            while line:
+                print(line.rstrip())
+                line = infile.readline()
+
+        # If optional key argument is provided, print out data product
+        # information line by line
+        elif args.key in keys:
+            headers = line.rstrip().split('|')
+            data_product_information_dict = dict.fromkeys(headers)
+
+            while line:
+                items = line.rstrip().split('|')
+                if args.key == items[0]:
+                    if len(items) == len(headers):
+                        for i, header in enumerate(headers):
+                            data_product_information_dict[header] = items[i]
+                    else:
+                        print("Number of headers and values do not match.")
+                line = infile.readline()
+
+            for key, value in data_product_information_dict.items():
+                print(f"{key:25} | {value:25}")
+        else:
+            print("Please provide a valid key, EXITING.")
+            if args.key is not None:
+                print("...First 10 potential keys in {} with string '{}': {}".
+                      format(args.file, args.key,
+                             list(filter(lambda k: args.key in k, keys))[:10]))
+            exit(1)
+
+        infile.close()
+    elif args.file in COMPOSITE_KEY_FILES:
+        # TODO (achao): Handle composite key for Services.catalog
+        pass
+    else:
+        print("Please provide a valid catalog filename.")
+        print("Valid files: {}".
+              format(PRIMARY_KEY_FILES + COMPOSITE_KEY_FILES))
+        exit(1)
+
+
 def update(args):
     """Updates column values for specified file and key."""
 
@@ -27,10 +122,15 @@ def update(args):
         outfile = open(args.file + '.out', 'w')
 
         # A. Get list of keys from file, confirm user provided key is valid
-        keys = get_keys(infile)
+        keys = get_keys(args.file)
         infile.seek(0)
-        if args.key not in keys[1:]:
-            print("Please provide a valid key.")
+        if args.key not in keys:
+            print("Please provide a valid key, EXITING.")
+            if args.key is not None:
+                print("...First 10 potential keys in {} with string '{}': {}".
+                      format(args.file, args.key,
+                             list(filter(lambda k: args.key in k, keys))[:10]))
+            exit(1)
 
         # B. Get list of headers from file, store value and index in dictionary
         header_list = infile.readline()
@@ -44,17 +144,18 @@ def update(args):
         cols = [col.lower() for col in args.cols_vals[::2]]
 
         if len(vals) != len(cols):  # Confirm same number of columns and values
-            print("Number of columns does not match number of values, exiting.")
+            print("Number of columns and values do not match, EXITING.")
             exit(1)
         elif len(cols) != len(set(cols)):  # Confirm no duplicate columns
-            print("Duplicate columns provided, exiting.")
+            print("Duplicate columns provided, EXITING.")
             exit(1)
         else:  # Confirm no invalid columns
             for col in cols:
                 if col not in header_dictionary.keys():
-                    print(f"'{col}' is not a valid column, exiting.")
-                    print("Valid columns are:", list(header_dictionary.keys())[1:])
-                exit(1)
+                    print(f"'{col}' is not a valid column, EXITING.")
+                    print("Valid columns are:",
+                          list(header_dictionary.keys())[1:])
+                    exit(1)
 
         cols_vals = zip(cols, vals)
 
@@ -66,8 +167,8 @@ def update(args):
             items = line.split('|')
             if args.key == items[0]:
                 for col_val in cols_vals:
-                    print("Updating column: {0:15} | [{1}] => [{2}].".format(
-                        col_val[0],
+                    print("Updating column: {0} | value: {1} => {2}.".format(
+                        col_val[0].upper(),
                         items[header_dictionary[col_val[0]]].rstrip(),
                         col_val[1]))
                     items[header_dictionary[col_val[0]]] = col_val[1]
@@ -80,6 +181,7 @@ def update(args):
 
         infile.close()
         outfile.close()
+
     elif args.file in COMPOSITE_KEY_FILES:
         # TODO (achao): Handle composite key for Services.catalog
         pass
@@ -90,60 +192,47 @@ def update(args):
         exit(1)
 
 
+def delete(args):
+    """Deletes a row based on key provided."""
 
-def read(args):
-    """Prints header and values for given key."""
+    if args.file in PRIMARY_KEY_FILES:
+        with open(args.file, 'r') as infile:
+            keys = get_keys(args.file)
 
-    # TODO (achao): Handle compound key for Services.catalog
-    infile = open(args.file, 'r')
+            if args.key not in keys:
+                print("Provided key: '{}' does not exist in {}, EXITING.".
+                      format(args.key, args.file))
+                exit(1)
+            lines = infile.readlines()
 
-    # Get list of keys from file and return cursor to start of file
-    keys = get_keys(infile)
-    infile.seek(0)
+        if input("Are you sure you want to delete '{}' from {}? (y/n) ".
+                 format(args.key, args.file)) != "y":
+            exit(1)
 
-    # If optional key argument is not provided, print all lines of file
-    line = infile.readline()
-    if args.key is None:
-        while line:
-            print(line.rstrip())
-            line = infile.readline()
+        with open(args.file, 'w') as infile:
+            for line in lines:
+                if line.split('|')[0] != args.key:
+                    infile.write(line)
+        print("Deleted '{}' from {}.".
+              format(args.key, args.file))
 
-    # If optional key argument is provided, print out data product
-    # information line by line
-    elif args.key in keys:
-        headers = line.rstrip().split('|')
-        data_product_information_dict = dict.fromkeys(headers)
+    elif args.file in COMPOSITE_KEY_FILES:
+        # TODO (achao): Handle composite key for Services.catalog
+        pass
 
-        while line:
-            items = line.rstrip().split('|')
-            if args.key == items[0]:
-                if len(items) == len(headers):
-                    for i, header in enumerate(headers):
-                        data_product_information_dict[header] = items[i]
-                else:
-                    print("Number of headers and values do not match.")
-            line = infile.readline()
-
-        for key, value in data_product_information_dict.items():
-            print(f"{key:25} | {value:25}")
     else:
-        print("Please provide a valid key.")
-
-    infile.close()
+        print("Please provide a valid catalog filename.")
+        print("Valid files: {}".
+              format(PRIMARY_KEY_FILES + COMPOSITE_KEY_FILES))
+        exit(1)
 
 
 def get_keys(file):
     """Returns keys for given file."""
-    
-    return [line.split('|')[0] for line in file.readlines()]
-
-
-def create():
-    """Adds key and items to file."""
-
-
-def delete():
-    """Deletes a row based on key provided."""
+    if file in PRIMARY_KEY_FILES:
+        with open(file, "r") as infile:
+            keys = [line.split("|")[0] for line in infile.readlines()][1:]
+    return keys
 
 
 def main():
@@ -154,10 +243,10 @@ def main():
         description='summary: command line tool to edit catalog files')
     subparsers = parser.add_subparsers(help='<sub-command> [-h] [<args>]')
 
-    # Sub-command Update
+    # Sub-command: Update
     parser_update = subparsers.add_parser(
-        'update',
-        help='Updates given column-value pairs for key row in file')
+        "update",
+        help="Updates column values for given file and key")
     parser_update.add_argument("file", help="filename to edit")
     parser_update.add_argument("key", help="primary key in provided file")
     parser_update.add_argument("cols_vals", nargs="+",
@@ -165,18 +254,34 @@ def main():
                                usage: [col1 val1 col2 val2 ...]")
     parser_update.set_defaults(func=update)
 
-    # Sub-command Read
+    # Sub-command: Read
     parser_read = subparsers.add_parser(
         'read',
-        help='Returns header and values for given key')
+        help='Reads file line by line unless a -k/--key is specified')
     parser_read.add_argument("file", help="filename to read")
     parser_read.add_argument(
         "-k", "--key", default=None, metavar='FOO',
         help="key in file to reference")
     parser_read.set_defaults(func=read)
 
+    # Sub-command: Add
+    parser_read = subparsers.add_parser(
+        'add',
+        help='Adds given to key to file')
+    parser_read.add_argument("file", help="filename to append key row to")
+    parser_read.add_argument("key", help="key in file to reference")
+    parser_read.set_defaults(func=add)
+
+    # Sub-command: Delete
+    parser_read = subparsers.add_parser(
+        'delete',
+        help='Deletes row in file for given key')
+    parser_read.add_argument("file", help="filename to delete key row from")
+    parser_read.add_argument("key", help="key in file to reference")
+    parser_read.set_defaults(func=delete)
+
     args = parser.parse_args(None if sys.argv[1:] else ['-h'])
-    args.func(args)
+    return args.func(args)
 
 
 if __name__ == "__main__":
